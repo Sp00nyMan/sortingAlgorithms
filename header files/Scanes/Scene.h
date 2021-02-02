bed38d6b21f67d2b2f987f3ede56777c27f03dd3
Sp00nyMan <mrstormproxdd@gmail.com>
2021-02-02 16:02:49 +0300

    Revert "Initial"
    This reverts commit 995a9e09ab1ad62d8cbdbd1f30856c910d558d00.

--- header files/Scanes/Scene.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "SFML/Graphics.hpp"
-#include "../Sortings/SortingMethod.h"
-
-class Scene
-{
-	virtual bool update(sf::RenderWindow& window, SortingMethod*& currentSorting, bool& fast, int& currentArraySize) = 0;
-	virtual void draw(sf::RenderWindow& window) = 0;
-};
--- header files/Sortings/Heap.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "SortingMethod.h"
-class Heap : public SortingMethod
-{
-public:
-	Heap(unsigned index);
-	Lines update(Lines& lines, sf::RenderWindow& window, Button& backButton) override;
-	Lines fastUpdate(Lines& lines, sf::RenderWindow& window, Button& backButton) override;
-};
